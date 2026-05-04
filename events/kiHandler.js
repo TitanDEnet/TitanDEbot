611eb9191c3463c382c68cb4754015da3b31d3ec
TitanDEnet <kiesowkumm@gmail.com>
2026-05-04 11:56:54 +0200

    Add files via upload

--- events/kiHandler.js
+++ events/kiHandler.js
@@ -24,7 +24,7 @@ async function frageGemini(text, userId) {
     const apiKey = process.env.GEMINI_API_KEY;
     const options = {
       hostname: 'generativelanguage.googleapis.com',
-      path: `/v1beta/models/gemini-2.0-flash:generateContent?key=${apiKey}`,
+      path: `/v1beta/models/gemini-1.5-flash:generateContent?key=${apiKey}`,
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
     };
